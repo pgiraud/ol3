eadfd19b7481b3042eab9d4f84d1e6250f7a5d71
Pierre GIRAUD <pierre.giraud@camptocamp.com>
2015-11-30 16:49:57 +0100

    Example for #4504

--- examples/select-features.js
+++ examples/select-features.js
@@ -2,25 +2,15 @@ goog.require('ol.Map');
 goog.require('ol.View');
 goog.require('ol.events.condition');
 goog.require('ol.format.GeoJSON');
+goog.require('ol.interaction.Draw');
 goog.require('ol.interaction.Select');
 goog.require('ol.layer.Tile');
 goog.require('ol.layer.Vector');
 goog.require('ol.source.MapQuest');
 goog.require('ol.source.Vector');
 
-var raster = new ol.layer.Tile({
-  source: new ol.source.MapQuest({layer: 'sat'})
-});
-
-var vector = new ol.layer.Vector({
-  source: new ol.source.Vector({
-    url: 'data/geojson/countries.geojson',
-    format: new ol.format.GeoJSON()
-  })
-});
 
 var map = new ol.Map({
-  layers: [raster, vector],
   target: 'map',
   view: new ol.View({
     center: [0, 0],
@@ -28,60 +18,20 @@ var map = new ol.Map({
   })
 });
 
-var select = null;  // ref to currently selected interaction
+map.addInteraction(new ol.interaction.Draw({
+    style: new ol.style.Style({
+        stroke: new ol.style.Stroke({
+            color: 'rgba(0, 0, 0, 0.5)',
+            lineDash: [10, 10],
+            width: 2
+        })
+    }),
+    type: 'Polygon',
+    source: new ol.source.Vector()
+}));
 
-// select interaction working on "singleclick"
-var selectSingleClick = new ol.interaction.Select();
-
-// select interaction working on "click"
-var selectClick = new ol.interaction.Select({
-  condition: ol.events.condition.click
-});
-
-// select interaction working on "pointermove"
-var selectPointerMove = new ol.interaction.Select({
-  condition: ol.events.condition.pointerMove
-});
-
-var selectAltClick = new ol.interaction.Select({
-  condition: function(mapBrowserEvent) {
-    return ol.events.condition.click(mapBrowserEvent) &&
-        ol.events.condition.altKeyOnly(mapBrowserEvent);
-  }
-});
-
-var selectElement = document.getElementById('type');
-
-var changeInteraction = function() {
-  if (select !== null) {
-    map.removeInteraction(select);
-  }
-  var value = selectElement.value;
-  if (value == 'singleclick') {
-    select = selectSingleClick;
-  } else if (value == 'click') {
-    select = selectClick;
-  } else if (value == 'pointermove') {
-    select = selectPointerMove;
-  } else if (value == 'altclick') {
-    select = selectAltClick;
-  } else {
-    select = null;
-  }
-  if (select !== null) {
-    map.addInteraction(select);
-    select.on('select', function(e) {
-      document.getElementById('status').innerText = '&nbsp;' +
-          e.target.getFeatures().getLength() +
-          ' selected features (last operation selected ' + e.selected.length +
-          ' and deselected ' + e.deselected.length + ' features)';
-    });
-  }
-};
-
-
-/**
- * onchange callback on the select element.
- */
-selectElement.onchange = changeInteraction;
-changeInteraction();
+map.addInteraction(new ol.interaction.Select({
+    filter: function(feature, layer) {
+        return false;
+    }
+}));
